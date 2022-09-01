09c56663442f2ebcf463856eb464d30eb17edd37
Tony Loehr <turingxo@gmail.com>
2022-09-01 12:49:25 -0700

    Update Api.swift

--- TriumphSwiftTest/TriumphSwiftTest/Api/Api.swift
+++ TriumphSwiftTest/TriumphSwiftTest/Api/Api.swift
@@ -11,4 +11,9 @@ class Api {
     static var User = UsersApi()
     static var Donations = DonationsApi()
     static var Organization = OrganizationApi()
+    
+    let postData = "username=Steve&password=123456".data(using: .utf8)  // Sensitive
+        //...
+    var request = URLRequest(url: url)
+    request.HTTPBody = postData
 }
